62b6fb3b3d764f8ed439dbd96309702ae2d584a8
Marilia Gama <mgama@opentable.com>
2024-10-17 20:58:21 -0700

    Add radio group and download

---
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.udacity
+
+import android.app.DownloadManager
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import com.udacity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var downloadID: Long = 0
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var pendingIntent: PendingIntent
+    private lateinit var action: NotificationCompat.Action
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        var selectedUrl = ""
+
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val selectedRadioButton: RadioButton = findViewById(checkedId)
+            selectedUrl = selectedRadioButton.tag.toString() // Get the URL from the selected radio button
+        }
+
+        binding.customButton.setOnClickListener {
+            download(selectedUrl)
+        }
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1) ?: 0
+
+
+            //DownloadManager.Query() is used to filter DownloadManager queries
+            val query = DownloadManager.Query()
+
+            query.setFilterById(id)
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+
+
+            val cursor = downloadManager.query(query)
+
+            if (cursor.moveToFirst()){
+                val test = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+
+                val status = cursor.getInt(test)
+
+                when (status) {
+                    DownloadManager.STATUS_SUCCESSFUL ->{
+                        Toast.makeText(context, "testing succes " + id, Toast.LENGTH_LONG).show()
+
+                    }
+                    DownloadManager.STATUS_FAILED -> {
+                        Toast.makeText(context, "testing fail " + id, Toast.LENGTH_LONG).show()
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun download(url: String) {
+        val request =
+            DownloadManager.Request(Uri.parse(url))
+                .setTitle(getString(R.string.app_name))
+                .setDescription(getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadID =
+            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channelId"
+    }
+}
